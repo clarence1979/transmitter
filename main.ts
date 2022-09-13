de04587f2a942bf13c6f29b967675285ba9a2d26
Clarence <47097322+clarence1979@users.noreply.github.com>
2022-09-14 00:57:46 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 radio.setGroup(1)
 basic.forever(function () {
     radio.sendValue("x", input.acceleration(Dimension.X))
-    basic.showIcon(IconNames.Yes)
+    basic.showString("x")
+    basic.showNumber(input.acceleration(Dimension.X))
+    basic.showString("y")
+    basic.showNumber(input.acceleration(Dimension.Y))
     radio.sendValue("y", input.acceleration(Dimension.Y))
 })
